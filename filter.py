ec79f520bd009555fa0b29319007e61998dbfb37
Ben Jourdan <ben@jourdan.me>
2018-03-11 01:36:15 +0000

    adding main

--- filter.py
+++ filter.py
@@ -13,7 +13,7 @@ def get_all_players(url=keywords.url):
 def get_enemies(url=keywords.url):
     player=player_GET(url)
     players=get_all_players(url)
-    print (player,players)
+
     enemies=[p for p in players  if p['id']!=player['id']]
     return enemies
 
@@ -21,7 +21,3 @@ def sort_enemies(enemies,key=closest,reverse=False):
     return sorted(enemies,key=key,reverse=reverse)
 
 
-enemies=get_enemies()
-enemies=sort_enemies(enemies)
-
-print (enemies)
