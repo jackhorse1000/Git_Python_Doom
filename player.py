d5352e7893392a04706376751cab35896d747416
jackhorsburgh <jackhorse1000@gmail.com>
2018-03-11 00:03:33 +0000

    a

--- player.py
+++ player.py
@@ -2,18 +2,23 @@ import requests
 import json
 import time
 
-url = "http://0.0.0.0:6001"
+url = "http://0.0.0.0:6002"
 
 
+def player_GET():
+    r = requests.get(url + "/api/player")
+    result = json.loads(r.text)
+    return result
 
-def player_GET(url):
+
+def players_GET():
 
     r = requests.get(url+"/api/players")
     result = json.loads(r.text)
     return result
 
 
-# print(player_GET("http://0.0.0.0:6001"))
+print(player_GET())
 
 def player_Action(type, amount ):
 
@@ -25,10 +30,12 @@ def player_Action(type, amount ):
 
 
 
-    data_json = json.dumps(data)
-    headers = {'content-type': 'application/json'}
-    r = requests.post(url+"/api/player/actions", data = data_json, headers=headers)
 
-for x in range(4):
-    time.sleep(3)
-    player_Action("http://0.0.0.0:6002", "turn-left", 28)
+
+#     data_json = json.dumps(data)
+#     headers = {'content-type': 'application/json'}
+#     r = requests.post(url+"/api/player/actions", data = data_json, headers=headers)
+#
+# for x in range(4):
+#     time.sleep(3)
+#     player_Action("http://0.0.0.0:6002", "turn-left", 28)
