a892bb904f6c1c776af191c047bfd4e67d4a412e
jackhorsburgh <jackhorse1000@gmail.com>
2018-03-11 00:37:16 +0000

    a

--- world.py
+++ world.py
@@ -1,16 +1,21 @@
 import requests
 import json
 
+url = "http://0.0.0.0:6002"
 
 
-
-def world_GET(url):
+def world_GET():
 
     r= requests.get(url+"/api/world")
-    result = json.loads(r.text())
+    result = json.loads(r.text)
     return result
 
+def los_GET(id1, id2):
+    r = requests.get(url + "/api/world/los/" + str(id1) + "/" + str(id2))
+    result = json.loads(r.text)
+    return result
 
+print(los_GET(0,0))
 print(world_GET())
 
 
